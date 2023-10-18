73855264dc7120b6c12de8d6c61bde91d85082f8
stephanedurette <Sdurette2688@conestogac.on.ca>
2023-10-18 03:18:08 -0400

    before wrappers and interfaces

--- Assets/Scripts/ShowRandomCard.cs
+++ Assets/Scripts/ShowRandomCard.cs
@@ -6,69 +6,32 @@ using UnityEngine.Events;
 public class ShowRandomCard : MonoBehaviour
 {
     public TextMeshProUGUI currentCardLabel;
-    public UnityEvent drawCard = new(); // We'll use this later
+    public UnityEvent drawCard;
 
-    static readonly System.Random rng = new();
-
-    enum Suit
+    ShowRandomCardImpl _showRandomCardImpl;
+    ShowRandomCardImpl ShowRandomCardImpl
     {
-        Spades, Hearts, Clubs, Diamonds
-    }
-
-    enum Rank
-    {
-        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
-        Jack, Queen, King
-    }
-
-    // Select a random element from an enum.
-    T RandomSelect<T>()
-    {
-        Array values = Enum.GetValues(typeof(T));
-        return (T)values.GetValue(rng.Next(values.Length));
-    }
-
-    Color SuitToColor(Suit suit)
-    {
-        switch (suit)
+        get
         {
-            case Suit.Spades:
-            case Suit.Clubs:
-                return Color.black;
-            case Suit.Hearts:
-            case Suit.Diamonds:
-                return Color.red;
-            default:
-                throw new InvalidOperationException($"Unknown suit: {suit}.");
+            if (_showRandomCardImpl == null)
+                _showRandomCardImpl = new(this.gameObject, currentCardLabel, () => drawCard?.Invoke());
+            
+            return _showRandomCardImpl;
         }
-    }
-
-    (string, Color) RandomCard()
-    {
-        Suit suit = RandomSelect<Suit>();
-        Rank rank = RandomSelect<Rank>();
-        return ($"{rank} of {suit}", SuitToColor(suit));
     }
 
     void Awake()
     {
-        // Good defensive practice.
-        if (currentCardLabel == null)
-        {
-            throw new MissingReferenceException(
-                "Missing Current Card Label in Show Random Card on " +
-                $"{this.name}.");
-        }
+        ShowRandomCardImpl.Awake();
     }
 
     void Start()
     {
-        currentCardLabel.text = "";
+        ShowRandomCardImpl.Start();
     }
 
     public void OnMouseDown()
     {
-        (currentCardLabel.text, currentCardLabel.color) = RandomCard();
-        drawCard?.Invoke();
+        ShowRandomCardImpl.OnMouseDown();
     }
 }
---
+++ Assets/Scripts/ShowRandomCardImpl.cs
@@ -0,0 +1,80 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShowRandomCardImpl
+{
+    TextMeshProUGUI currentCardLabel;
+    GameObject parent;
+    Action drawCard;
+
+    static readonly System.Random rng = new();
+
+    public ShowRandomCardImpl(GameObject parent, TextMeshProUGUI currentCardLabel, Action drawCard)
+    {
+        this.parent = parent;
+        this.currentCardLabel = currentCardLabel;
+        this.drawCard = drawCard;
+    }
+
+    public enum Suit
+    {
+        Spades, Hearts, Clubs, Diamonds
+    }
+
+    public enum Rank
+    {
+        Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+        Jack, Queen, King
+    }
+
+    T RandomSelect<T>()
+    {
+        Array values = Enum.GetValues(typeof(T));
+        return (T)values.GetValue(rng.Next(values.Length));
+    }
+
+    Color SuitToColor(Suit suit)
+    {
+        switch (suit)
+        {
+            case Suit.Spades:
+            case Suit.Clubs:
+                return Color.black;
+            case Suit.Hearts:
+            case Suit.Diamonds:
+                return Color.red;
+            default:
+                throw new InvalidOperationException($"Unknown suit: {suit}.");
+        }
+    }
+
+    (string, Color) RandomCard()
+    {
+        Suit suit = RandomSelect<Suit>();
+        Rank rank = RandomSelect<Rank>();
+        return ($"{rank} of {suit}", SuitToColor(suit));
+    }
+
+    public void Awake()
+    {
+        if (currentCardLabel == null)
+        {
+            throw new MissingReferenceException(
+                "Missing Current Card Label in Show Random Card on " +
+                $"{parent.name}.");
+        }
+    }
+
+    public void Start()
+    {
+        currentCardLabel.text = "";
+    }
+
+    public void OnMouseDown()
+    {
+        (currentCardLabel.text, currentCardLabel.color) = RandomCard();
+        drawCard?.Invoke();
+    }
+}
